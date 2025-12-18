714c9365bd70fcb9bb580d1d183229a6305de3f7
Orne <ornellameolans@gmail.com>
2025-12-17 22:56:20 -0300

    Fix deploy paths

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,6 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // 👇 ESTA LÍNEA ES LA CLAVE. Debe coincidir con el nombre de tu repositorio.
+  base: "/CreaTuLanding-Meolans/",
 })
