9eff54d28509342c46d0ea6d5e8b94d94596840b
Orne <ornellameolans@gmail.com>
2026-01-22 21:45:24 -0300

    arreglo

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/CreaTuLanding-Meolans/", // 👈 AGREGA ESTA LÍNEA
+  base: "/CreaTuLanding-Meolans/", // 👈 ESTO ES FUNDAMENTAL
 })
